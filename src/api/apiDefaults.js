93ce429ba63e765ae64b2b37e613bccff679ef09
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-06-18 02:40:29 +0200

    update base url to production api

--- src/api/apiDefaults.js
+++ src/api/apiDefaults.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
-// axios.defaults.baseURL = "https://socialvibe-api-32609e33d535.herokuapp.com";
+// axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = "https://socialvibe-api-32609e33d535.herokuapp.com";
 
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem('user.access');
