6283aa06355f5ca880928eda1fb63fce3265f9d4
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-06-25 17:02:04 +0200

    Add Profile Info test

---
+++ src/__tests__/components/ProfileInfo.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import ProfileInfo from "../../components/ProfileInfo";
+import { PostsProvider } from "../../context/PostsContext";
+import { ToastProvider } from "../../context/ToastContext";
+import { FriendshipProvider } from "../../context/FriendshipContext";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+
+jest.mock("../../context/UserContext", () => ({
+  useUser: jest.fn(() => ({
+    user: {
+      id: "4f38f403-24b0-42a6-ae40-a8b9637c8789",
+      name: "zohaib",
+      email: "zohaib16@gmail.com",
+      friendsCount: 5,
+    },
+  })),
+}));
+
+describe("ProfileInfo", () => {
+  it("renders profile information and actions correctly", async () => {
+    const userId = "4f38f403-24b0-42a6-ae40-a8b9637c8789"; // Replace with a valid userId for testing
+
+    render(
+      <MemoryRouter initialEntries={[`/profile/${userId}`]}>
+        <ToastProvider>
+          <PostsProvider>
+            <FriendshipProvider>
+              <Routes>
+                <Route path="/profile/:userId" element={<ProfileInfo />} />
+              </Routes>
+            </FriendshipProvider>
+          </PostsProvider>
+        </ToastProvider>
+      </MemoryRouter>
+    );
+
+    await waitFor(() => expect(screen.getByText("zohaib")).toBeInTheDocument());
+
+    expect(screen.getByText("zohaib")).toBeInTheDocument();
+    expect(screen.getByText("zohaib16@gmail.com")).toBeInTheDocument();
+    expect(screen.getByText("Friends")).toBeInTheDocument();
+    expect(screen.getByText("5")).toBeInTheDocument();
+    expect(screen.queryByText("Edit")).toBeInTheDocument();
+    expect(screen.queryByText("Logout")).toBeInTheDocument();
+  });
+});
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -19,6 +19,31 @@ export const handlers = [
       })
     );
   }),
+  rest.get(`${baseURL}/api/friends/:userId`, (req, res, ctx) => {
+    const { userId } = req.params;
+    if (userId === "4f38f403-24b0-42a6-ae40-a8b9637c8789") {
+      return res(
+        ctx.json({
+          friends: [
+            {
+              id: "123456",
+              name: "Friend 1",
+            },
+            {
+              id: "789012",
+              name: "Friend 2",
+            },
+            {
+              id: "345678",
+              name: "Friend 3",
+            },
+          ],
+        })
+      );
+    } else {
+      return res(ctx.status(404));
+    }
+  }),
 
   rest.post(`${baseURL}/logout/`, (req, res, ctx) => {
     return res(ctx.status(200));
@@ -55,6 +80,14 @@ export const handlers = [
     );
   }),
 
+  rest.post(`${baseURL}/api/friend-request/:userId`, (req, res, ctx) => {
+    const { userId } = req.params;
+    if (userId === "4f38f403-24b0-42a6-ae40-a8b9637c8789") {
+      return res(ctx.status(200));
+    } else {
+      return res(ctx.status(404));
+    }
+  }),
   rest.get(`${baseURL}/api/posts/:postId/`, (req, res, ctx) => {
     return res(
       ctx.json({
@@ -99,5 +132,66 @@ export const handlers = [
         },
       })
     );
+  }),
+  rest.get(`${baseURL}/api/posts/profile/:userId`, (req, res, ctx) => {
+    const { userId } = req.params;
+    if (userId === "4f38f403-24b0-42a6-ae40-a8b9637c8789") {
+      return res(
+        ctx.json({
+          posts: [
+            {
+              id: "9cf520ba-47c9-48db-9aa8-860adbbb5a8f",
+              body: "My First Post",
+              likes_count: 0,
+              comments_count: 1,
+              created_by: {
+                id: "4a234166-27b0-46ca-9301-67154fae2d7a",
+                name: "Ali",
+                email: "ali16@gmail.com",
+                friends_count: 1,
+                posts_count: 1,
+                get_avatar:
+                  "https://res.cloudinary.com/dceudxuqq/image/upload/v1718720898/media/profile_gs8tic.png",
+              },
+              created_at_formatted: "0\u00a0minutes",
+              comments: [
+                {
+                  id: "17da08ab-f68a-4029-929e-c71a667b8bc6",
+                  body: "Heelow",
+                  created_by: {
+                    id: "4a234166-27b0-46ca-9301-67154fae2d7a",
+                    name: "Ali",
+                    email: "ali16@gmail.com",
+                    friends_count: 1,
+                    posts_count: 1,
+                    get_avatar:
+                      "https://res.cloudinary.com/dceudxuqq/image/upload/v1718720898/media/profile_gs8tic.png",
+                  },
+                  created_at_formatted: "0\u00a0minutes",
+                },
+              ],
+              attachments: [
+                {
+                  id: "151e5ab4-1e2c-4909-80f7-50f87dd31827",
+                  get_image:
+                    "https://res.cloudinary.com/dceudxuqq/image/upload/v1/media/post_attachments/sea_turtle_underwater_art-wallpaper-1920x1080_ucq8tq",
+                },
+              ],
+            },
+          ],
+          user: {
+            id: "4f38f403-24b0-42a6-ae40-a8b9637c8789",
+            name: "zohaib",
+            email: "zohaib16@gmail.com",
+            avatar:
+              "https://res.cloudinary.com/dceudxuqq/image/upload/v1/media/avatars/sea_turtle_underwater_art-wallpaper-1920x1080_qpftvs",
+            friends_count: 2,
+            posts_count: 5,
+          },
+        })
+      );
+    } else {
+      return res(ctx.status(404));
+    }
   }),
 ];
